18b3df4b2c91b96234a61015d8c818db413591ee
gupettine <gupettine@gmail.com>
2024-07-09 12:12:44 -0300

    refactor: optimize logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 import { globalStyles } from "@/styles/global";
 import { Container, Header } from "@/styles/pages/app";
 
+import Image from "next/image";
 import logoImg from "@/assets/logo.svg";
 
 globalStyles()
@@ -10,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
