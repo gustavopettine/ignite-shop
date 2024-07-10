e14a05bb068f09d765673010a2c17f24f6ef06e6
gupettine <gupettine@gmail.com>
2024-07-10 14:44:42 -0300

    feature: add ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import { stripe } from "@/lib/stripe"
 import Stripe from "stripe"
 import Image from "next/image"
@@ -42,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -61,6 +61,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
