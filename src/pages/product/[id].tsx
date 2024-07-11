9f7dbc29597e8cbe539cf23fb435ae9dd69b3ccf
gupettine <gupettine@gmail.com>
2024-07-11 14:31:55 -0300

    feature: create product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>T-shirt X</h1>
+
+        <span>$79,90</span>
+
+        <p>As nice as a hug, our products are made from the purest and finest Brazilian cotton, ideal for climates from North to South. All colors are 100% cotton, except the mixed gray ones, which are 88% cotton and 12% polyester.</p>
+
+        <button>Buy now</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
